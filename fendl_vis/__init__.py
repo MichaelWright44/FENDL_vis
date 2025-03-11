545c76e783aa74dba8a7c8fb00df1df7ff914f44
Michael Wright <michaelwright4231@gmail.com>
2025-03-10 17:57:01 -0700

    removed cli relics

--- fendl_vis/__init__.py
+++ fendl_vis/__init__.py
@@ -5,6 +5,5 @@ FENDL_vis - Library for loading and visualizing ENDF data from FENDL.
 __version__ = "0.1.0"
 
 from .loader import EndfLoader
-from .viewer import EndfViewer
 from .plotter import EndfPlotter
 from .gui import EndfGui, run_gui 
